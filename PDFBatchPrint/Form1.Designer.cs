e7c410de0002f9b8c30bf2c7c7500b946916ab63
Donatien Grolaux <dgrolaux@grayswindle.com>
2023-06-22 14:56:30 +0200

    First commit with code

---
+++ PDFBatchPrint/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PDFBatchPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.PDFFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Print = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboPrinterNames = new System.Windows.Forms.ComboBox();
+            this.buttonDeselected = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPrint.Location = new System.Drawing.Point(286, 309);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrint.TabIndex = 0;
+            this.buttonPrint.Text = "Print...";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrintClick);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowDrop = true;
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PDFFile,
+            this.Print,
+            this.Status});
+            this.dataGrid.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(715, 280);
+            this.dataGrid.TabIndex = 8;
+            // 
+            // PDFFile
+            // 
+            this.PDFFile.FillWeight = 99.49239F;
+            this.PDFFile.HeaderText = "PDF File";
+            this.PDFFile.Name = "PDFFile";
+            this.PDFFile.ReadOnly = true;
+            // 
+            // Print
+            // 
+            this.Print.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Print.FillWeight = 99.49239F;
+            this.Print.HeaderText = "Print?";
+            this.Print.Name = "Print";
+            this.Print.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Print.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Print.Width = 59;
+            // 
+            // Status
+            // 
+            this.Status.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            this.Status.Width = 62;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClear.Location = new System.Drawing.Point(610, 309);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(117, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear List";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClearClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Printer";
+            // 
+            // comboPrinterNames
+            // 
+            this.comboPrinterNames.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboPrinterNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboPrinterNames.FormattingEnabled = true;
+            this.comboPrinterNames.Location = new System.Drawing.Point(55, 311);
+            this.comboPrinterNames.Name = "comboPrinterNames";
+            this.comboPrinterNames.Size = new System.Drawing.Size(225, 21);
+            this.comboPrinterNames.TabIndex = 10;
+            // 
+            // buttonDeselected
+            // 
+            this.buttonDeselected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDeselected.Location = new System.Drawing.Point(487, 309);
+            this.buttonDeselected.Name = "buttonDeselected";
+            this.buttonDeselected.Size = new System.Drawing.Size(117, 23);
+            this.buttonDeselected.TabIndex = 12;
+            this.buttonDeselected.Text = "Clear Deselected";
+            this.buttonDeselected.UseVisualStyleBackColor = true;
+            this.buttonDeselected.Click += new System.EventHandler(this.buttonDeselected_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(367, 309);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 13;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 356);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonDeselected);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboPrinterNames);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.buttonPrint);
+            this.MinimumSize = new System.Drawing.Size(612, 200);
+            this.Name = "Form1";
+            this.Text = "Batch Print PDF Files";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboPrinterNames;
+        private System.Windows.Forms.Button buttonDeselected;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PDFFile;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Print;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
+
---
+++ PDFBatchPrint/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using PdfiumViewer;
+
+namespace PDFBatchPrint
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GlobalLock(IntPtr hMem);
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalUnlock(IntPtr hMem);
+        [DllImport("kernel32.dll")]
+        static extern bool GlobalFree(IntPtr hMem);
+        [DllImport("winspool.Drv", EntryPoint = "DocumentPropertiesW", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+        static extern int DocumentProperties(IntPtr hwnd, IntPtr hPrinter, [MarshalAs(UnmanagedType.LPWStr)] string pDeviceName, IntPtr pDevModeOutput, IntPtr pDevModeInput, int fMode);
+
+        private const int DM_PROMPT = 4;
+        private const int DM_OUT_BUFFER = 2;
+        private const int DM_IN_BUFFER = 8;
+        PrinterSettings printerSettings = null;
+        private bool stopping = false;
+        private int printingIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGrid.AllowDrop = true;
+            this.dataGrid.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.dataGrid.DragDrop += new DragEventHandler(Form1_DragDrop);
+
+            foreach (var printer in PrinterSettings.InstalledPrinters)
+                this.comboPrinterNames.Items.Add(printer);
+
+            printerSettings = new PrinterSettings();
+            this.comboPrinterNames.SelectedItem = printerSettings.PrinterName;
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (file.ToLower().EndsWith(".pdf"))
+                {
+                    dataGrid.Rows.Add(file, true, "");
+                }
+            }
+        }
+
+        private void buttonPrintClick(object sender, EventArgs e)
+        {
+            int count = 0;
+            for (int i = 0; i < dataGrid.Rows.Count; i++)
+            {
+                var row = dataGrid.Rows[i];
+                if ((bool)row.Cells[1].Value == true) count++;
+            }
+            if (count == 0)
+            {
+                MessageBox.Show(this, "There are no files selected to print.\nPlease drag n' drop some files into the list box above.");
+                return;
+            }
+            else
+            {
+                printerSettings.PrinterName = (string)this.comboPrinterNames.SelectedItem;
+                IntPtr hDevMode = printerSettings.GetHdevmode(printerSettings.DefaultPageSettings);
+                IntPtr pDevMode = GlobalLock(hDevMode);
+                int sizeNeeded = DocumentProperties(this.Handle, IntPtr.Zero, printerSettings.PrinterName, IntPtr.Zero, pDevMode, 0);
+                IntPtr devModeData = Marshal.AllocHGlobal(sizeNeeded);
+                long userChoice = DocumentProperties(this.Handle, IntPtr.Zero, printerSettings.PrinterName, devModeData, pDevMode, DM_IN_BUFFER | DM_PROMPT | DM_OUT_BUFFER);
+                long IDOK = (long)DialogResult.OK;
+                if (userChoice == IDOK)
+                {
+                    printerSettings.SetHdevmode(devModeData);
+                    printerSettings.DefaultPageSettings.SetHdevmode(devModeData);
+                    this.printingIndex = 0;
+                    this.stopping = false;
+                    buttonPrint.Enabled = false;
+                    buttonPrint.Text = "Printing...";
+                    buttonStop.Enabled = true;
+                    buttonStop.Text = "Stop";
+                    print();
+                }
+                GlobalUnlock(hDevMode);
+                GlobalFree(hDevMode);
+                Marshal.FreeHGlobal(devModeData);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClearClick(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "Are you sure you want to clear the list of items to be printed?", "Confirm Clear", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.dataGrid.Rows.Clear();
+        }
+        private void print()
+        {
+            Form1 self = this;
+            if (!this.stopping)
+            {
+                for (; this.printingIndex < dataGrid.Rows.Count; this.printingIndex++)
+                {
+                    var row = dataGrid.Rows[printingIndex];
+                    if (!(bool)row.Cells[1].Value) continue;
+                    string filename = row.Cells[0].Value as string;
+                    row.Cells[2].Value = "Printing...";
+                    PrintThread pt = new PrintThread(filename, printerSettings); ;
+                    ThreadStart starter = new ThreadStart(pt.Print);
+                    starter += () =>
+                    {
+                        self.Invoke(new Action(() =>
+                        {
+                            row.Cells[1].Value = false;
+                            if (pt.Result)
+                            {
+                                row.Cells[2].Value = "Printed";
+                            } else
+                            {
+                                row.Cells[2].Value = "Failed";
+                            }
+                            print();
+
+                        }));
+                    };
+                    Thread t = new Thread(starter);
+                    t.Start();
+                    return;
+                }
+            }
+            // reset UI
+            this.stopping = false;
+            buttonPrint.Enabled = true;
+            buttonPrint.Text = "Print...";
+            buttonStop.Enabled = false;
+            buttonStop.Text = "Stop";
+        }
+
+        private void buttonDeselected_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "Are you sure you want to remove deselected items?", "Confirm Clear", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                for (int i = dataGrid.Rows.Count - 1; i >= 0; i--)
+                {
+                    var row = dataGrid.Rows[i];
+                    if (!(bool)row.Cells[1].Value)
+                    {
+                        dataGrid.Rows.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonStop.Text = "Stopping";
+            buttonStop.Enabled = false;
+            this.stopping = true;
+        }
+    }
+
+    class PrintThread
+    {
+        private string filename;
+        private PrinterSettings settings;
+        public bool Result { get; set; }
+        private EventWaitHandle wh = new AutoResetEvent(false);
+
+        public PrintThread(string filename, PrinterSettings settings)
+        {
+            this.filename = filename;
+            this.settings = settings;
+        }
+
+        public void Print()
+        {
+            try
+            {
+
+                // Now print the PDF document
+                using (var document = PdfDocument.Load(filename))
+                {
+                    using (var printDocument = document.CreatePrintDocument())
+                    {
+                        printDocument.PrinterSettings = settings;
+                        printDocument.PrintController = new StandardPrintController();
+                        printDocument.EndPrint += finish;
+                        printDocument.Print();
+                        wh.WaitOne();
+                    }
+                }
+                Result = true;
+            }
+            catch
+            {
+                Result = false;
+            }
+        }
+
+        private void finish(object sender, PrintEventArgs e)
+        {
+            wh.Set();
+        }
+    }
+}
